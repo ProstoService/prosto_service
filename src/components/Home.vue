715d6444a5574daa250d2378171caeb6ba498375
Петр Шургалин <petr.shurgalin@gmail.com>
2018-11-14 10:07:54 +0300

    main page template

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,24 @@
 <template>
-<div>
-  <h1>PROSTO SERVICE</h1>
-  <router-link to="/about">
-    <sui-button basic>
-      О нас
-    </sui-button>
-  </router-link>
-</div>
+  <div class="landing">
+    <div class="centered">
+      <div class="ui one column grid">
+        <div class="center aligned one column row">
+          <div class="column">
+            <h1 style="font-style: bold:">PROSTO SERVICE</h1>
+          </div>
+        </div>
+        <div class="center aligned one column row">
+          <div class="column">
+            <router-link to="/about">
+              <sui-button basic>
+                О нас
+              </sui-button>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,3 +26,17 @@ export default {
   name: 'HomePage'
 }
 </script>
+
+<style scoped>
+  .centered {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+  }
+
+  .landing {
+    background-image: url('../../static/images/landing.jpeg');
+    background-size: cover;
+  }
+</style>
