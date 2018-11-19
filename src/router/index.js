d361e6c4f217c3042d985161614044758894f15b
Петр Шургалин <petr.shurgalin@gmail.com>
2018-11-19 12:18:05 +0300

    title

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,10 @@ const router = new Router(
       {
         path: '/',
         name: 'Home',
-        component: () => import('@/components/Home')
+        component: () => import('@/components/Home'),
+        meta: {
+          title: ''
+        }
       },
       {
         path: '/',
@@ -19,22 +22,34 @@ const router = new Router(
           {
             path: 'about',
             name: 'About',
-            component: () => import('@/components/About')
+            component: () => import('@/components/About'),
+            meta: {
+              title: 'О нас'
+            }
           },
           {
             path: 'services',
             name: 'Service',
-            component: () => import('@/components/Services')
+            component: () => import('@/components/Services'),
+            meta: {
+              title: 'Услуги'
+            }
           },
           {
             path: 'products',
             name: 'Products',
-            component: () => import('@/components/Products')
+            component: () => import('@/components/Products'),
+            meta: {
+              title: 'Товары'
+            }
           },
           {
             path: 'contacts',
             name: 'Contacts',
-            component: () => import('@/components/Contacts')
+            component: () => import('@/components/Contacts'),
+            meta: {
+              title: 'Контакты'
+            }
           }
         ]
       },
@@ -45,13 +60,27 @@ const router = new Router(
           {
             path: '',
             name: 'NotFound',
-            component: () => import('@/components/NotFound')
+            component: () => import('@/components/NotFound'),
+            meta: {
+              title: 'Страница не найдена'
+            }
           }
         ]
       }
     ]
   }
 )
+
+const globalTitle = 'Prosto Service'
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title === '') {
+    document.title = globalTitle
+  } else {
+    document.title = to.meta.title + ' | ' + globalTitle
+  }
+  next()
+})
 
 router.beforeResolve((to, from, next) => {
   if (to.name) {
